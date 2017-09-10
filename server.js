b907fa273e57e907d29ab96c68453a14ada90dcf
gingin77 <ginniehench@gmail.com>
2017-09-09 21:37:45 -0400

    after following a simpler set of instructions, I'm able to connect to a get route in postman. See https://codeforgeek.com/2015/08/restful-api-node-mongodb/

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const router = express.Router()
 const port = process.env.PORT || 3000
 const mongoose = require('mongoose')
 const Exercise = require('./models/exerciseSchema')
-const bodyParser = require('body-parser')
 
-// mongoose instance connection url connection
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/Tododb')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-
-app.use(function (req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
+router.get('/', function(req,res){
+  res.json({
+    "error": false,
+    "message": "Hello World"})
 })
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app.use('/', router)
 
-const routes = require('./routes/activities')
-routes(app)//register the route
+// // mongoose instance connection url connection
+// mongoose.Promise = global.Promise
+// mongoose.connect('mongodb://localhost/Tododb')
+//
+//
+// app.use(function (req, res) {
+//   res.status(404).send({url: req.originalUrl + ' not found'})
+// })
+//
+//
+//
+// const routes = require('./routes/activities')
+// routes(app)//register the route
 
 // const statsRouter = require('./routes/stats')
 // statsRouter(app)
@@ -27,6 +38,4 @@ routes(app)//register the route
 // app.use('/stats', statsRouter)
 
 app.listen(port)
-
-
 console.log('exercise list RESTful API server started on: ' + port)
